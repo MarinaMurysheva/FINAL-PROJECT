c15ecc1b8c80d517ef2bfd059e08b34c8665cb9e
muryshevamarina <marilz@bk.ru>
2022-09-30 23:10:11 +0500

    checking the array

--- programme/Program.cs
+++ programme/Program.cs
@@ -5,4 +5,15 @@ string[] arrayStart = new string[14] {"Hello", "world", "!", "My", "name", "is",
 string[] arrayFinal = new string[arrayStart.Length];
 void ShortArray(string[] arrayStart, string[] arrayFinal)
 
+{
+    int count = 0;
+    for (int i = 0; i < arrayStart.Length; i++)
+    {
+    if(arrayStart[i].Length <= 3)
+        {
+        arrayFinal[count] = arrayStart[i];
+        count++;
+        }
+    }
+}
 
